304c1b49f00092d7c1e52924a9dc4b4313405c5d
KostiaZhuk <zhukkostia88@gmail.com>
2016-10-06 18:46:03 +0300

    version 1

---
+++ database/migrations/2016_10_04_152700_create_tables.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Topics', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('topicname',100)->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('Blocks', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('topicid')->unsigned();
+            $table->foreign('topicid')->references('id')->on('Topics')->onDelete('cascade');
+            $table->string('title',100)->unique();  
+            $table->longText('content');         
+            $table->string('imagePath',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Blocks');
+        Schema::drop('Topics');
+    }
+}
